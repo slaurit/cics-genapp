643418e91a032154f1c9731b9d507ac96a52ec06
Meena <111979384+Meenadesu@users.noreply.github.com>
2024-10-29 14:00:46 +0000

    Update LGACDB01.cbl

--- cics-genapp/base/src/COBOL/LGACDB01.cbl
+++ cics-genapp/base/src/COBOL/LGACDB01.cbl
@@ -26,7 +26,7 @@
       *  SAJ DEVELOP
       *  CRP 07/10/2023
       *  MEENA DEMO TESTM DEMO
-      * MEENA FEATURE1 DEMO 
+      * MEENA FEATURE1
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. LGACDB01.
